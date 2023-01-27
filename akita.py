4a29185d974efd9271558c90311524dcc83d6fd5
João Morado <joaomorado@gmail.com>
2023-01-27 17:06:34 +0000

    updates do watchdog

--- akita.py
+++ akita.py
@@ -4,13 +4,14 @@ from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
 
 
-class Akita(PatternMatchingEventHandler):
+class FileHandler(PatternMatchingEventHandler):
     """
     Watchdog that sniffs and guards a filesystem.
     """
 
     def __init__(self, patterns: str | list[str] = ["*.nc"], ignore_patterns: str | list[str] | None = None,
                  ignore_directories: bool = True, case_sensitive: bool = True):
+
         super().__init__(patterns=patterns,
                          ignore_patterns=ignore_patterns,
                          ignore_directories=ignore_directories,
@@ -29,18 +30,35 @@ class Akita(PatternMatchingEventHandler):
         print(event)
 
 
+class Akita:
+    def __init__(self, watch_dir: str):
+        self.watch_dir = watch_dir
+        self.observer = Observer()
+
+    def run(self, patterns: str | list[str] = ["*.nc"], ignore_patterns: str | list[str] | None = None,
+            ignore_directories: bool = True, case_sensitive: bool = True):
+
+        event_handler = FileHandler(patterns=patterns,
+                                    ignore_patterns=ignore_patterns,
+                                    ignore_directories=ignore_directories,
+                                    case_sensitive=case_sensitive)
+
+        self.observer.schedule(event_handler, self.watch_dir, recursive=True)
+        self.observer.start()
+
+        try:
+            while True:
+                time.sleep(1)
+        finally:
+            self.observer.stop()
+
+        self.observer.join()
+
+
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
 
-    event_handler = Akita()
-    observer = Observer()
-    observer.schedule(event_handler, "./", recursive=True)
-    observer.start()
-
-    try:
-        while True:
-            time.sleep(1)
-    finally:
-        observer.stop()
+    akita_watchdog = Akita(watch_dir="./")
+    akita_watchdog.run()
