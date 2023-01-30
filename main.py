50ba0d7d39dd0ae657dcd20d7a46ba52d5ce9b90
João Morado <joaomorado@gmail.com>
2023-01-30 15:09:36 +0000

    proto-main

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ from etl.extract import setup_extract_from_server
 
 if __name__ == "__main__":
     logging.basicConfig(
-                        format='%(asctime)s %(levelname)-8s %(message)s',
-                        level=logging.INFO,
-                        datefmt='%Y-%m-%d %H:%M:%S')
+        format='%(levelname)s | %(asctime)s | %(message)s',
+        level=logging.INFO,
+        datefmt='%Y-%m-%d %H:%M:%S')
+
+    # Set up the actions
     setup_extract_from_server()
 
     akita = Akita(path="/home/joaomorado/Desktop/workspace/test_dir")
