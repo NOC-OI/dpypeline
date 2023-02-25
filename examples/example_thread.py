36a559f30d923dd9cb62602596d5990d416f7521
João Morado <joaomorado@gmail.com>
2023-02-25 13:44:15 +0000

    improvements to pipeline

--- examples/example_thread.py
+++ examples/example_thread.py
@@ -1,8 +1,6 @@
 """Run the ETL pipeline explicitly defining everything."""
 import logging
-import os
 
-import numpy as np
 import xarray as xr
 
 from dpypeline.akita.core import Akita
@@ -12,51 +10,13 @@ from dpypeline.etl_pipeline.thread_pipeline import ThreadPipeline
 from dpypeline.event_consumer.core import EventConsumer
 from dpypeline.filesystems.object_store import ObjectStoreS3
 
-
-def clean_dataset(
-    dataset: xr.Dataset,
-    fill_value=np.nan,
-    supression_value: float = 1e20,
-    threshold: float = 1e-6,
-    *args,
-    **kwargs,
-) -> str:
-    """Clean a dataset."""
-    fill_value = fill_value if fill_value is not None else np.NaN
-    supression_value = supression_value if supression_value is not None else np.NaN
-
-    for var in dataset.keys():
-        tmp = dataset[var].load()
-        tmp.values = tmp.fillna(fill_value)
-        tmp.values = xr.where(
-            np.abs(tmp - supression_value) < threshold, fill_value, tmp
-        )
-        tmp.encoding["_FillValue"] = fill_value
-
-    cached_file = f"{os.environ['CACHE_DIR']}/transformed_{dataset.encoding['source'].rsplit('/', 1)[-1].rsplit('.', 1)[-2]}.nc"
-    dataset.to_netcdf(cached_file)
-
-    return cached_file
-
-
-def to_zarr(dataset: xr.Dataset, *args, **kwargs):
-    return dataset.to_zarr(*args, **kwargs)
-
-
-def clean_cache_dir(_):
-    import glob
-
-    file_list = glob.glob(
-        os.path.join(os.environ["CACHE_DIR"], "") + "*.nc", recursive=True
-    )
-    for file in file_list:
-        try:
-            os.remove(file)
-        except OSError:
-            logging.info(f"Failed to delete {file}")
-
-    return
-
+from .thread_pipeline_tasks import (
+    clean_cache_dir,
+    clean_dataset,
+    combine_first_with_template,
+    fix_name_vars,
+    to_zarr,
+)
 
 if __name__ == "__main__":
     logging.basicConfig(
@@ -66,8 +26,8 @@ if __name__ == "__main__":
     )
     # Create Akita
     akita_dep = get_akita_dependencies(
-        path="./",
-        patterns=["*.nc"],
+        path="/gws/nopw/j04/nemo_vol1/ORCA0083-N006/means/",
+        patterns=["**/ORCA*-N06_*m*T.nc"],
         ignore_patterns=None,
         ignore_directories=True,
         case_sensitive=True,
@@ -83,26 +43,39 @@ if __name__ == "__main__":
     event_consumer = EventConsumer(queue=akita.queue, job_producer=etl_pipeline)
 
     # Create the jasmin instance
+    logging.info("Jasmin OS")
     jasmin = ObjectStoreS3(
         anon=False, store_credentials_json="jasmin_object_store_credentials.json"
     )
-    bucket = "dpypline-test"
+    bucket = "n06-dataset"
     # jasmin.rm("dpypline-test/*", recursive=True)
     #   jasmin.mkdir(bucket)
 
     # Define the jobs and respective tasks
+    # 1. Open the data set
+    # 2. Fix variables' names
+    # 3. Clean the dataset
+    # 4. Combine dataset with template
+    # 5. Send to zarr
+    # 6. Clean cache directory
     job = Job(name="send_to_jasmin_OS")
-    job.add_task(Task(function=xr.open_dataset))
-    job.add_task(Task(function=clean_dataset))
     job.add_task(
         Task(
             function=xr.open_dataset,
             kwargs={"chunks": {"x": 577, "y": 577, "time_counter": 1, "deptht": 5}},
         )
     )
+    job.add_task(Task(function=fix_name_vars))
+    job.add_task(Task(function=clean_dataset))
+    job.add_task(Task(function=combine_first_with_template))
     job.add_task(
         Task(
-            function=to_zarr, kwargs={"store": jasmin.get_mapper(bucket + "/n06.zarr")}
+            function=to_zarr,
+            kwargs={
+                "store": jasmin.get_mapper(bucket + "/n06.zarr"),
+                "mode": "a",
+                "append_dim": "time_counter",
+            },
         )
     )
     job.add_task(Task(function=clean_cache_dir))
---
+++ examples/thread_pipeline_tasks.py
@@ -0,0 +1,202 @@
+"""Tasks for use by thread-based data pipelines."""
+import logging
+import os
+from difflib import SequenceMatcher
+from typing import Iterable, Sequence
+
+import numpy as np
+import xarray as xr
+
+
+def clean_dataset(
+    dataset: xr.Dataset,
+    fill_value=np.nan,
+    supression_value: float = 1e20,
+    threshold: float = 1e-6,
+    *args,
+    **kwargs,
+) -> xr.Dataset:
+    """Clean a dataset."""
+    fill_value = fill_value if fill_value is not None else np.NaN
+    supression_value = supression_value if supression_value is not None else np.NaN
+
+    for var in dataset.keys():
+        tmp = dataset[var].load()
+        tmp.values = tmp.fillna(fill_value)
+        tmp.values = xr.where(
+            np.abs(tmp - supression_value) < threshold, fill_value, tmp
+        )
+        tmp.encoding["_FillValue"] = fill_value
+        tmp.encoding["missing_value"] = fill_value
+
+    # cached_file = f"{os.environ['CACHE_DIR']}/transformed_{dataset.encoding['source'].rsplit('/', 1)[-1].rsplit('.', 1)[-2]}.nc"
+    # dataset.to_netcdf(cached_file)
+
+    return dataset
+
+
+def to_zarr(dataset: xr.Dataset, *args, **kwargs):
+    """to_zarr wrapper"""
+    return dataset.to_zarr(*args, **kwargs)
+
+
+def clean_cache_dir(_):
+    """Clean the cache directory."""
+    import glob
+
+    file_list = glob.glob(
+        os.path.join(os.environ["CACHE_DIR"], "") + "*.nc", recursive=True
+    )
+    for file in file_list:
+        try:
+            os.remove(file)
+        except OSError:
+            logging.info(f"Failed to delete {file}")
+
+    return
+
+
+def create_template(
+    ds: xr.Dataset,
+    drop_dims: Iterable = None,
+    drop_vars: Iterable = None,
+    output_file: str = "template.nc",
+) -> xr.Dataset:
+    """
+    Create a dataset template.
+
+    Parameters
+    ----------
+    ds
+        Dataset from which to create a template.
+    drop_dims
+        Dimensions to drop from the template.
+    drop_vars
+        Variables to drop from the template.
+    output_file
+        Name of the output NetCDF file.
+
+    Returns
+    -------
+        Template dataset.
+    """
+    # Set values of all variables to np.nan
+    template = xr.Dataset(ds.data_vars, ds.coords, ds.attrs)
+    for var in template.keys():
+        template[var].values = xr.where(np.isnan(template[var]), template[var], np.nan)
+        template[var].encoding["_FillValue"] = np.nan
+        template[var].encoding["missing_value"] = np.nan
+
+    # Drop dimensions
+    if drop_dims is not None:
+        template = template.drop_dims(drop_dims)
+
+    # Drop vars
+    if drop_vars is not None:
+        template = template.drop_vars(drop_vars)
+
+    # Set all time coordinates to np.NaT
+    """
+    for coord in template.coords.keys():
+        if coord.startswith("time"):
+            template [xr.where(template.coords[coord] == np.datetime64('NaT'), template.coords[coord], np.datetime64('NaT'))
+    """
+    template.to_netcdf(output_file)
+
+    return template
+
+
+def similar(a: Sequence, b: Sequence) -> float:
+    """
+    Return the similarity of two sequences.
+
+    Parameters
+    ----------
+    a
+        Sequence 1.
+    b
+        Seqence 2.
+
+    Returns
+    -------
+        The similarity of two sequences.
+    """
+    return SequenceMatcher(None, a, b).ratio()
+
+
+def create_reference_names_dict(ds: xr.Dataset) -> dict:
+    """
+    Create a dictionary of reference names.
+
+    Parameters
+    ----------
+    ds
+        Dataset from which to create a reference names dictionary.
+
+    Returns
+    -------
+        Reference names dictionary.
+    """
+    ref_names_dict = {}
+    for var in ds.keys():
+        ref_names_dict[ds[var].attrs["long_name"].lower()] = var
+
+    return ref_names_dict
+
+
+def fix_name_vars(ds: xr.Dataset, reference_names: dict):
+    """
+    Fix the names of variables in a dataset.
+
+    Parameters
+    ----------
+    ds
+        Dataset to fix.
+    reference_names
+        Dictionary of reference names.
+
+    Returns
+    -------
+        Dataset with fixed names.
+    """
+    logging.info("Fixing variables' names")
+    rename_dict = {}
+    for var in ds.keys():
+        found = False
+        long_name = ds[var].attrs["long_name"]
+        for ref_var in reference_names.keys():
+            similarity = similar(long_name.lower(), ref_var.lower())
+            if similarity > 0.9:
+                logging.info("")
+                logging.info(
+                    f"Renaming var: {long_name.lower()} = {ref_var.lower()} with similarity {similarity}."
+                )
+                logging.info(f"Renaming var: {var} -> {reference_names[ref_var]}")
+                rename_dict[var] = reference_names[ref_var]
+                found = True
+                break
+
+        if not found:
+            logging.info(f"{long_name} has no correspondence in reference names.")
+
+    ds = ds.rename_vars(rename_dict)
+
+    return ds
+
+
+def combine_first_with_template(ds: xr.Dataset, template: xr.Dataset):
+    """
+    Combine a dataset with a template.
+
+    Parameters
+    ----------
+    ds
+        Dataset to combine.
+    template
+        Template dataset.
+
+    Returns
+    -------
+        Combined dataset.
+    """
+    return template.combine_first(ds)
